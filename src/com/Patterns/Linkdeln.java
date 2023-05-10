68726bebadc887f563ceaeea92aaae3e1ed8cefb
yekol <yekol@LAPTOP-O0F0P2QQ>
2023-05-10 10:36:41 +0530

    y.pradeep

---
+++ src/com/Patterns/Linkdeln.java
@@ -0,0 +1,20 @@
+package com.Patterns;
+
+import java.util.Scanner;
+
+public class Linkdeln {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1=" HELLO";
+		String s2=" World";
+		String s3 = s1.concat(s2);
+		System.out.println(s3);
+		
+		
+		
+		
+
+	}
+
+}
--- src/com/Patterns/RemoveSpaceString.java
+++ src/com/Patterns/RemoveSpaceString.java
@@ -1,14 +1,18 @@
 package com.Patterns;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class RemoveSpaceString {
 
 	public static void main(String[] args) {
+		
 		String str= " j a va pr o g ram m";
 		String a = str.replaceAll(" ", "");
-		System.out.println(" The replace String:"+a);
-	}
+		System.out.println(" Before replace string:"+str);
+		System.out.println(" The after replace String:"+a);
+		
+}
 }
 
 		
